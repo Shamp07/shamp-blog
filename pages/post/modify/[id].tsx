961261d2147665b7687cf1e573ca301ff4cfb61e
Shamp <blog.shamp@gmail.com>
2021-02-09 11:02:39 +0900

    fix: 관리자 수정 화면에 못들어가는 버그 픽스

--- pages/post/modify/[id].tsx
+++ pages/post/modify/[id].tsx
@@ -10,7 +10,7 @@ const ModifyPost: NextPage = () => {
   const { toggleAlertModal } = AlertStore;
   const router = useRouter();
 
-  if (!userData || userData.adminFl) {
+  if (!userData || !userData.adminFl) {
     router.push('/').then(() => {
       toggleAlertModal('글 수정 권한이 없습니다.');
     });
