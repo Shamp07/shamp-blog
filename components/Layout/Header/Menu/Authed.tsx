0b2afe77eede98fff78690918ef5aa82acca8472
Shamp07 <blog.shamp@gmail.com>
2021-11-16 22:33:58 +0900

    feat: 로그인 이후 헤더 아바타 및 레이아웃 수정

--- components/Layout/Header/Menu/Authed.tsx
+++ components/Layout/Header/Menu/Authed.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import Avatar from '@mui/material/Avatar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 import dsPalette from '@constants/ds-palette';
+import stores from '@stores';
 
-const Authed = () => (
-  <Root>
-    <Link href="/write" passHref>
-      <Posting>글 작성</Posting>
-    </Link>
-    <div>
-      <div>이미지</div>
+const Authed = () => {
+  const { signStore } = stores();
+  const { userData } = signStore;
+  if (!userData) return null;
 
-    </div>
-  </Root>
-);
+  return (
+    <Root>
+      <Link href="/write" passHref>
+        <Posting>글 작성</Posting>
+      </Link>
+      <Profile>
+        <AvatarIcon>{userData.name.substring(0, 1)}</AvatarIcon>
+        <FontAwesomeIcon icon={faSortDown} />
+      </Profile>
+    </Root>
+  );
+};
 
 const Root = styled.div({
   display: 'flex',
+  alignItems: 'center',
 });
 
 const Posting = styled.a({
@@ -25,14 +36,35 @@ const Posting = styled.a({
   alignItems: 'center',
   fontSize: '1rem',
   cursor: 'pointer',
-  background: dsPalette.themePrimary.toString(),
-  color: dsPalette.themeWhite.toString(),
+  background: dsPalette.themeWhite.toString(),
+  color: dsPalette.themePrimary.toString(),
   paddingLeft: '1rem',
   paddingRight: '1rem',
   height: '2rem',
   borderRadius: '1rem',
+  border: `1px solid ${dsPalette.themePrimary.toString()}`,
   fontWeight: 700,
   textDecoration: 'none',
+
+  '&:hover': {
+    color: dsPalette.typeWhite.toString(),
+    background: dsPalette.themePrimary.toString(),
+    transition: 'all .125s ease-in 0s',
+  },
+});
+
+const Profile = styled.div({
+  display: 'flex',
+  cursor: 'pointer',
+  marginLeft: '1rem',
+  alignItems: 'center',
+});
+
+const AvatarIcon = styled(Avatar)({
+  marginRight: '.5rem',
+  '&&&': {
+    fontFamily: 'inherit',
+  },
 });
 
 export default Authed;
