c845724df8510c66aa670116bbac62b6988a0695
Shamp07 <blog.shamp@gmail.com>
2021-11-23 22:34:48 +0900

    feat: PostCard 수정 및 Home 레이아웃 수정

--- components/home/PostCard.tsx
+++ components/home/PostCard.tsx
@@ -3,32 +3,43 @@ import styled from '@emotion/styled';
 
 import dsPalette from '@constants/ds-palette';
 
-const PostCard = () => {
-  return (
-    <Root>
-      <Title>Title</Title>
-      <Content>Content</Content>
-      <Footer>
-        <div>3일 전 · 2개의 댓글</div>
-        <div>좋아요 3</div>
-      </Footer>
-    </Root>
-  );
-};
+const PostCard = () => (
+  <Root>
+    <Section>
+      <Title>기본기의 중요성</Title>
+      <Content>
+        Code Splitting (코드분할) 이란? 코드 분할(Code Splitting)은
+        SPA의 성능을 향상시키는 방법입니다.
+        싱글 페이지 애플리케이션(Single
+        ddsadsadsadsadasdsadsadsad
+      </Content>
+    </Section>
+    <Footer>
+      <div>3일 전 · 2개의 댓글</div>
+      <div>좋아요 3</div>
+    </Footer>
+  </Root>
+);
 
 const Root = styled.div({
   width: '20rem',
   backgroundColor: dsPalette.themeWhite.toString(),
   borderRadius: '4px',
+  margin: '1rem',
 });
 
 const Title = styled.h4({
   fontSize: '1rem',
-  lineHeight: 1.5,
 });
 
 const Content = styled.p({
+  fontSize: '.875rem',
+});
+
+const Section = styled.div({
   padding: '1rem',
+  lineHeight: 1.5,
+
 });
 
 const Footer = styled.div({
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Grid from '@mui/material/Grid';
 
 import PostCard from '@components/Home/PostCard';
+import { MediaQuery } from '@constants/styles';
+import * as T from '@types';
 
-const Home = () => {
-  return (
-    <div>
-      <PostCard />
-    </div>
-  );
-};
+const Home = () => (
+  <Root>
+    <Wrapper>
+      <Inner>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </Inner>
+    </Wrapper>
+  </Root>
+);
+
+const Root = styled.div({
+  width: '1728px',
+  margin: '0 auto',
+
+  [MediaQuery[T.Device.DESKTOP]]: {
+    width: '1376px',
+  },
+
+  [MediaQuery[T.Device.LAPTOP]]: {
+    width: '1024px',
+  },
+
+  [MediaQuery[T.Device.TABLET]]: {
+    width: 'calc(100% - 2rem)',
+  },
+});
+
+const Wrapper = styled.div({
+  flex: '1 1 0%',
+});
+
+const Inner = styled.div({
+  display: 'flex',
+  margin: '-1rem',
+  flexWrap: 'wrap',
+});
 
 export default Home;
