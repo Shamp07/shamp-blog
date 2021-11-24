e7658baf7a0705ae96596cf827b0c38f66ccfcd8
Shamp07 <blog.shamp@gmail.com>
2021-11-24 21:58:55 +0900

    feat: 포스트 카드 UI/UX 수정

--- components/home/PostCard.tsx
+++ components/home/PostCard.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import styled from '@emotion/styled';
 
+import tempo from '@assets/images/tempo.gif';
 import dsPalette from '@constants/ds-palette';
 
 const PostCard = () => (
   <Root>
-    <Section>
-      <Title>기본기의 중요성</Title>
-      <Content>
-        Code Splitting (코드분할) 이란? 코드 분할(Code Splitting)은
-        SPA의 성능을 향상시키는 방법입니다.
-        싱글 페이지 애플리케이션(Single
-        ddsadsadsadsadasdsadsadsad
-      </Content>
-    </Section>
+    <Link href="/post/1" passHref>
+      <Section>
+        <Image
+          src={tempo}
+          width="100%"
+          height="167"
+        />
+        <Inner>
+          <Title>Title</Title>
+          <Content>
+            Content
+          </Content>
+        </Inner>
+      </Section>
+    </Link>
     <Footer>
       <div>3일 전 · 2개의 댓글</div>
       <div>좋아요 3</div>
@@ -26,20 +35,37 @@ const Root = styled.div({
   backgroundColor: dsPalette.themeWhite.toString(),
   borderRadius: '4px',
   margin: '1rem',
+  overflow: 'hidden',
 });
 
 const Title = styled.h4({
   fontSize: '1rem',
+  margin: '0 0 .25rem',
 });
+
+const Inner = styled.div({
+  padding: '1rem',
+})
 
 const Content = styled.p({
   fontSize: '.875rem',
+  margin: '0 0 1.5rem',
+  height: '3.9375rem',
+  wordBreak: 'break-word',
+  overflowWrap: 'break-word',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  webkitLineClamp: '3',
+  webkitBoxOrient: 'vertical',
 });
 
-const Section = styled.div({
-  padding: '1rem',
+const Section = styled.a({
+  display: 'flex',
+  color: dsPalette.typePrimary.toString(),
+  textDecoration: 'none',
   lineHeight: 1.5,
-
+  flex: '1 1 0%',
+  flexDirection: 'column',
 });
 
 const Footer = styled.div({
