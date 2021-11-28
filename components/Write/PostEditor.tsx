0443c9ad48cb35b459c224ec75aacfcd451b7088
Shamp07 <blog.shamp@gmail.com>
2021-11-28 22:58:30 +0900

    feat: 에디터 커스텀 스타일 추가

--- components/Write/PostEditor.tsx
+++ components/Write/PostEditor.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 const PostEditor = () => (
-  <Editor
-    initialValue="hello react editor world!"
-    previewStyle="vertical"
-    height="600px"
-    initialEditType="markdown"
-    useCommandShortcut
-  />
+  <Root>
+    <Editor
+      initialValue="hello react editor world!"
+      previewStyle="vertical"
+      height="600px"
+      initialEditType="markdown"
+      useCommandShortcut
+    />
+  </Root>
 );
+
+const Root = styled.div({
+
+  '&&& *': {
+    fontFamily: 'inherit',
+  },
+
+  '.toastui-editor-contents': {
+    fontSize: '1.125rem',
+    lineHeight: '1.7',
+  },
+
+  '.toastui-editor-contents h1': {
+    lineHeight: '1.5',
+    fontSize: '2.5rem',
+    border: 0,
+  },
+
+  '.toastui-editor-contents h2': {
+    lineHeight: '1.5',
+    fontSize: '2rem',
+    border: 0,
+  },
+});
 
 export default PostEditor;
