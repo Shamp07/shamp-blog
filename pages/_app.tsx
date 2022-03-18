89f5c74ed0c3cc9ab250a63d26d21239ab906103
Shamp07 <blog.shamp@gmail.com>
2022-03-18 20:57:17 +0900

    feat(ga): GA 페이지 뷰 추적 추가 및 App 컴포넌트 FC 로 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,73 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import App, { AppContext } from 'next/app';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
+import * as T from '@types';
 import stores from '@stores';
 import Layout from '@components/Layout';
-import * as T from '@types';
+import { pageView } from '@utilities/ga';
 
 config.autoAddCss = false;
 
 const queryClient = new QueryClient();
 
-class CustomApp extends App {
-  static async getInitialProps(context: AppContext) {
-    if (typeof window === 'undefined') {
-      axios.defaults.headers.Cookie = context.ctx.req?.headers.cookie ?? '';
-    }
+const MyApp = ({ Component, pageProps, initialMobxState }: T.MyAppProps) => {
+  if (typeof window !== 'undefined') stores(initialMobxState);
 
-    const store = stores();
-    await store.signStore.authCheck();
+  const router = useRouter();
 
-    const appProps = await App.getInitialProps(context);
-    return {
-      ...appProps,
-      initialMobxState: store,
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageView(url);
     };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Head>
+        <title>Shamp Blog</title>
+        <meta
+          name="viewport"
+          content="user-scalable=no, width=device-width, initial-scale=1.0"
+        />
+        <meta property="title" content="Shamp Blog" key="title" />
+        <meta property="og:title" content="Shamp Blog" key="og-title" />
+        <meta name="description" content="저의 개발스토리와 일상을 공유하는 블로그입니다." key="description" />
+        <meta name="og:description" content="저의 개발스토리와 일상을 공유하는 블로그입니다." key="og-description" />
+        <meta property="og:type" content="website" key="og-type" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta
+          property="og:image"
+          content="https://ko.gravatar.com/userimage/201440531/c7eb1d92694dfbcf00f7a79ea61811bf.png?size=512"
+          key="og-image"
+        />
+        <meta property="og:site_name" content="Shamp Blog" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
+    </>
+  );
+};
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  if (typeof window === 'undefined') {
+    axios.defaults.headers.Cookie = context.ctx.req?.headers.cookie ?? '';
   }
 
-  constructor(props: T.MyAppProps) {
-    super(props);
-    if (typeof window !== 'undefined') {
-      stores(props.initialMobxState);
-    }
-  }
+  const store = stores();
+  await store.signStore.authCheck();
 
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <title>Shamp Blog</title>
-          <meta
-            name="viewport"
-            content="user-scalable=no, width=device-width, initial-scale=1.0"
-          />
-          <meta property="title" content="Shamp Blog" key="title" />
-          <meta property="og:title" content="Shamp Blog" key="og-title" />
-          <meta name="description" content="저의 개발스토리와 일상을 공유하는 블로그입니다." key="description" />
-          <meta name="og:description" content="저의 개발스토리와 일상을 공유하는 블로그입니다." key="og-description" />
-          <meta property="og:type" content="website" key="og-type" />
-          <meta property="og:locale" content="ko_KR" />
-          <meta
-            property="og:image"
-            content="https://ko.gravatar.com/userimage/201440531/c7eb1d92694dfbcf00f7a79ea61811bf.png?size=512"
-            key="og-image"
-          />
-          <meta property="og:site_name" content="Shamp Blog" />
-        </Head>
-        <QueryClientProvider client={queryClient}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </QueryClientProvider>
-      </>
-    );
-  }
-}
+  const appProps = await App.getInitialProps(context);
+  return {
+    ...appProps,
+    initialMobxState: store,
+  };
+};
 
-export default CustomApp;
+export default MyApp;
